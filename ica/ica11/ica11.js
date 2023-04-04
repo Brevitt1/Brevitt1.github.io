66af7f95e16dc4315ced0d0b3e2a5e3f0c26753f
Brevitt1 <brli1998@colorado.edu>
2023-04-04 10:33:24 -0600

    ica11

---
+++ ica/ica11/ica11.js
@@ -0,0 +1,31 @@
+const button = document.querySelector("#js-new-quote");
+button.addEventListener('click', getQuote);
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+ async function getQuote(){
+    //console.log("It works poop?");
+    try {
+        const response = await fetch(endpoint)
+        if (!response.ok) {
+            throw Error(response.statusText)
+        }
+        const json = await response.json();
+       // console.log(json.question);
+       displayQuote(json.question)
+    }
+    catch (err) {
+        console.log(err);
+        alert('Failed to fetch new trivia');
+
+    }
+}
+
+function displayQuote(quote) {
+    const quoteText = document.querySelector("#js-quote-text");
+    quoteText.textContent = quote;
+
+}
+
+
+getQuote();
